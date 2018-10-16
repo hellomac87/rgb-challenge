968d60bf2b8721bcd6320bc7ee5011ba9f64686c
hellomac87 <hellomac87@gmail.com>
2018-10-16 19:05:25 +0900

    신이시어 정년 이것이 제가 짠 코드란 말입니까?

--- javascript.js
+++ javascript.js
@@ -1,50 +1,83 @@
-function game(){
+function game() {
     this.score = 0; // 기본 score
-    this.scoreElem = document.getElementById("score"); 
+    this.answer = 0;
+    this.scoreElem = document.getElementById("score");
+    this.overlayScoreElem = document.getElementById("overlayScore");
     this.circles = document.querySelectorAll(".answers li");
     this.question = document.getElementById("question");
+    this.nextBtn = document.getElementById("nextBtn");
+    this.againBtn = document.getElementById("againBtn");
+    this.wrongDiv = document.querySelector('.wrong');
+    this.correctDiv = document.querySelector(".correct");
+    this.overlayDiv = document.querySelector(".overlay");
 
-    this.init = function(){
-        this.inputColor();
-        this.setRGB();
-        this.setScore();
-    }
+    this.nextBtn.addEventListener('click', (e) => {
+        // next 버튼 클릭시
+        this.newStage();
+        this.correctDiv.style.display = 'none';
+        this.overlayDiv.style.display = "none";
+        this.circles.forEach(circle => {
+            circle.classList.remove('show');
+        });
+    });
+    this.againBtn.addEventListener("click", e => {
+        // play again 버튼 클릭시
+        this.newStage();
+        this.wrongDiv.style.display = "none";
+        this.overlayDiv.style.display = "none";
+        this.circles.forEach(circle => {
+          circle.classList.remove("show");
+        });
+    });
 
-    this.inputColor = function(){
-        for (let i = 0; i < this.circles.length; i++) {
-            this.circles[i].style.backgroundColor = this.createRandomColor();
-            this.circles[i].addEventListener('click', this.clickColor);
-        }
-    }
-    this.setRGB = function(){
-        const questionRgb = this.circles[Math.floor(Math.random() * 3)];
-        this.question.textContent = `${questionRgb.style.backgroundColor}`;
-    }
+    this.circles.forEach((circle, index) => {
+        circle.addEventListener("click", e => {
+            if (this.answer === index) {
+                // ok
+                this.correctDiv.style.display = 'block';
+                this.overlayDiv.style.display = "block";
+                this.handleScore();
+            } else {
+                // no
+                console.log("no");
+                this.wrongDiv.style.display = "block";
+                this.overlayDiv.style.display = "block";
+            }
+            circle.classList.add("show");
+        });
+    });
 
-    this.createRandomColor = function(){
-        const cadidate = '0123456789ABCDEF';
-        let colorCode = '#'
+  this.newStage = function() {
+    const options = [
+      this.createRandomColor(),
+      this.createRandomColor(),
+      this.createRandomColor()
+    ];
 
-        for (let i = 0; i < 6; i++) {
-            const index = Math.floor(Math.random() * cadidate.length);;
-            colorCode += cadidate[index];
-        }
-        return colorCode;
-    }
+    this.answer = Math.floor(Math.random() * 3);
 
-    this.setScore = function(){
-        this.scoreElem.textContent = this.score;
-    }
+    this.circles.forEach((circle, index) => {
+        circle.style.backgroundColor = options[index];
+        circle.classList.remove('active');
+    });
 
-    this.clickColor = function(){
-        console.log(this.style.backgroundColor);
-        console.log(this.question);
-    }
+    this.question.textContent = options[this.answer];
+  };
+
+  this.createRandomColor = function() {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+  };
+
+  this.handleScore = function(){
+      this.score += 100;
+      this.scoreElem.textContent = this.score;
+      this.overlayScoreElem.textContent = this.score;
+  }
 }
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    const game1 = new game();
-
-    game1.init();
+document.addEventListener("DOMContentLoaded", function() {
+  const game1 = new game();
+  game1.newStage();
 });
